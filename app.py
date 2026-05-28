f95c648920fecc9a21d3b1999d4cdd9f4c4e0754
VenkataSailaja <64885898+VenkataSailaja@users.noreply.github.com>
2026-05-28 17:05:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ v1 = client.CoreV1Api()
 # List pods in default namespace
 pods = v1.list_namespaced_pod(namespace="default")
 
-print("Pods in default namespace:")
+print("Pods in default namespace")
 
 for pod in pods.items:
     print(pod.metadata.name)
