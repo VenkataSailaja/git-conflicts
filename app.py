ce3df1c71d6bd9308151c2891b4a21f7c024e167
VenkataSailaja <sailajaperi2000@gmail.com>
2026-05-28 17:08:57 +0530

    done some changes to file

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ config.load_kube_config()
 v1 = client.CoreV1Api()
 
 # List pods in default namespace
-pods = v1.list_namespaced_pod(namespace="default")
+pods = v1.list_namespaced_pod(namespace="")
 
-print("Pods in default namespace")
+print("Pods in default namespace:")
 
 for pod in pods.items:
     print(pod.metadata.name)
